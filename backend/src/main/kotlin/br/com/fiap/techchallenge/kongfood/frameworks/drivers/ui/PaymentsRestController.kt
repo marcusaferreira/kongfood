b844bc145a565ab35a429b2667f54e3a566cb460
Marcus Ferreira <marcus_aferreira@hotmail.com>
2024-01-30 08:58:24 -0300

    define summary of the new operations

--- backend/src/main/kotlin/br/com/fiap/techchallenge/kongfood/frameworks/drivers/ui/PaymentsRestController.kt
+++ backend/src/main/kotlin/br/com/fiap/techchallenge/kongfood/frameworks/drivers/ui/PaymentsRestController.kt
@@ -3,6 +3,7 @@ package br.com.fiap.techchallenge.kongfood.frameworks.drivers.ui
 import br.com.fiap.techchallenge.kongfood.domain.payment.interfaces.adapters.controllers.PaymentService
 import br.com.fiap.techchallenge.kongfood.domain.payment.interfaces.adapters.models.PaymentResponse
 import br.com.fiap.techchallenge.kongfood.frameworks.drivers.ui.dto.PaymentNotification
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -16,16 +17,19 @@ class PaymentsRestController(
     val paymentService: PaymentService
 ) {
 
+    @Operation(summary = "Notify the status of a payment, this is for getting the return from the payment gateway")
     @PostMapping("/notify/{orderID}")
     fun notify(@PathVariable("orderID") orderID: String, @RequestBody payment: PaymentNotification) {
         paymentService.notify(orderID, payment)
     }
 
+    @Operation(summary = "Request a payment to the payment gateway. This is temporary, in the future this will be done by the payment gateway")
     @PostMapping("/request/{orderID}")
     fun requestPayment(@PathVariable("orderID") orderID: String): PaymentResponse {
         return paymentService.request(orderID)
     }
 
+    @Operation(summary = "Verify the status of a payment, this is for getting the return from the payment gateway")
     @GetMapping("/verify/{orderID}")
     fun verifyPayment(@PathVariable("orderID") orderID: String): PaymentResponse {
         return paymentService.verify(orderID)
